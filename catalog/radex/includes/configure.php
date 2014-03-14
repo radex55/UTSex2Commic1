45441e8c69eae25a70c9d3fe7069c2af82122b1a
Aria Deki Saputra <deki.ashter@hotmail.com>
2014-03-14 14:55:57 +0700

    testing
    Os commerce

---
+++ catalog/radex/includes/configure.php
@@ -0,0 +1,36 @@
+<?php
+  define('HTTP_SERVER', 'http://localhost');
+  define('HTTP_CATALOG_SERVER', 'http://localhost');
+  define('HTTPS_CATALOG_SERVER', 'http://localhost');
+  define('ENABLE_SSL_CATALOG', 'false');
+  define('DIR_FS_DOCUMENT_ROOT', 'E:/websitekeren/www/UTSex2Commic1/catalog/');
+  define('DIR_WS_ADMIN', '/UTSex2Commic1/catalog/radex/');
+  define('DIR_FS_ADMIN', 'E:/websitekeren/www/UTSex2Commic1/catalog/radex/');
+  define('DIR_WS_CATALOG', '/UTSex2Commic1/catalog/');
+  define('DIR_WS_HTTPS_CATALOG', '/UTSex2Commic1/catalog/');
+  define('DIR_FS_CATALOG', 'E:/websitekeren/www/UTSex2Commic1/catalog/');
+  define('DIR_WS_IMAGES', 'images/');
+  define('DIR_WS_ICONS', DIR_WS_IMAGES . 'icons/');
+  define('DIR_WS_CATALOG_IMAGES', DIR_WS_CATALOG . 'images/');
+  define('DIR_WS_INCLUDES', 'includes/');
+  define('DIR_WS_BOXES', DIR_WS_INCLUDES . 'boxes/');
+  define('DIR_WS_FUNCTIONS', DIR_WS_INCLUDES . 'functions/');
+  define('DIR_WS_CLASSES', DIR_WS_INCLUDES . 'classes/');
+  define('DIR_WS_MODULES', DIR_WS_INCLUDES . 'modules/');
+  define('DIR_WS_LANGUAGES', DIR_WS_INCLUDES . 'languages/');
+  define('DIR_WS_CATALOG_LANGUAGES', DIR_WS_CATALOG . 'includes/languages/');
+  define('DIR_FS_CATALOG_LANGUAGES', DIR_FS_CATALOG . 'includes/languages/');
+  define('DIR_FS_CATALOG_IMAGES', DIR_FS_CATALOG . 'images/');
+  define('DIR_FS_CATALOG_MODULES', DIR_FS_CATALOG . 'includes/modules/');
+  define('DIR_FS_BACKUP', DIR_FS_ADMIN . 'backups/');
+  define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
+  define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
+
+  define('DB_SERVER', 'localhost');
+  define('DB_SERVER_USERNAME', 'root');
+  define('DB_SERVER_PASSWORD', '');
+  define('DB_DATABASE', 'dq');
+  define('USE_PCONNECT', 'false');
+  define('STORE_SESSIONS', 'mysql');
+  define('CFG_TIME_ZONE', 'Asia/Jakarta');
+?>
